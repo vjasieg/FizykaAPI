27402cd1b5f2cfb424acae9c878f89f4a09d4dd5
vjasieg <vjsaieg>
2021-03-27 19:47:15 +0100

    matplotlib and flask test

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import flask
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/', methods=['GET'])
+def home():
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
+    return "gitara siema"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.run()
